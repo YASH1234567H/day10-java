e641b12e0e78a43cd040a2012378c0fda3adae72
Yashwant <anyname6281@gmail.com>
2024-12-11 10:36:04 +0530

    Add files via upload

---
+++ non static.java
@@ -0,0 +1,12 @@
+class main
+{
+    static string s()
+    {
+        return"HELLO WORLD";
+    }
+    public static void main(String args[])
+    {
+        String res=s();
+        System.out.println(res);
+    }
+}
