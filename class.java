27b083949dd5a64cfd3ba6ddcb544b7d570c6c37
Yashwant <anyname6281@gmail.com>
2024-12-11 10:26:27 +0530

    Add files via upload

---
+++ class.java
@@ -0,0 +1,11 @@
+class a{
+    int a1=5;
+}
+class main{
+    public static void main(String args[]){
+        a r=new a();// new is used for instance variable
+        System.out.println(r.a1);
+
+    }
+
+}
